90c13e8c45aeb7540aa96c706d34948dc002fb9f
JAHEERHUSSAIN24 <jaheerhussain20022001@gmail.com>
2026-02-20 09:40:41 +0530

    Create todayjs.html.js

---
+++ todayjs.html.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>today</title>
+</head>
+<body>
+    <script src="todayjs.js"></script>
+</body>
+</html>
+<!-- js -->
+let name = 'Arun';
+
+console.log('Hello ${name}, your age is ${underfined}.')
+
+
+console.log('5' + 3)
+console.log('5' - 3)
